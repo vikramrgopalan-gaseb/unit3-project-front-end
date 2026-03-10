97934de6b67d6074e4e28c87eb0ca5d13f1f0260
scifi <leith.huddleston@gmail.com>
2026-03-10 14:46:45 -0600

    added another comment, navbar, and app.jsx routes.

--- APPcepted-front-end/src/App.jsx
+++ APPcepted-front-end/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState, useContext } from 'react'
 import { Route, Routes } from 'react-router'
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import SignUpForm from './components/SignUpForm/SignUpForm'
+
 
 function App() {
 
 
   return (
     <>
-      
+      <NavBar />
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes>
     </>
   )
 }
