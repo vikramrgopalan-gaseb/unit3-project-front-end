334826766fa483e27a0a610529a0d020877cd91a
scifi <leith.huddleston@gmail.com>
2026-03-12 19:25:45 -0600

    added the view/edit class as user page, added link functionality to homepage, made the fetch function in app.jsx a useEffect and passed it to edit and delete class functions, also wrote enroll and disenroll services in homeServices.js. a couple minor bug fixes too

--- APPcepted-front-end/src/App.jsx
+++ APPcepted-front-end/src/App.jsx
@@ -3,50 +3,53 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import HomePage from './components/HomePage/HomePage'
+import EditUserClass from './components/EditUserClass/EditUserClass'
 import SignUpForm from './components/SignUpForm/SignUpForm'
 import SignInForm from './components/SignInForm/SignInForm'
 import TopicForm from './components/TopicForm/TopicForm';
-import createClass from './components/CreateClassForm/CreateClassForm'
+import CreateClass from './components/CreateClassForm/CreateClassForm'
 import MyClasses from './components/MyClasses/MyClasses'
 import MyTopics from './components/MyTopics/MyTopics'
 import EditMyClass from './components/EditMyClass/EditMyClass'
 
 import { fetchClasses, fetchTopics } from './services/listServices'
-
-const [classes, setClasses] = useState([])
-const [selectedClass, setSelectedClass] = useState({})
-
-const [topics, setTopics] = useState([])
-const [selectedTopic, setSelectedTopic] = useState({})
-
-const fetchClassList = async () => {
-  const fetchedClasses = await fetchClasses()
-  setClasses(fetchedClasses)
-}
-
-fetchClassList()
-
-const fetchTopicList = async () => {
-  const fetchedTopics = await fetchTopics()
-  setTopics(fetchedTopics)
-}
-
-fetchTopicList()
+import { useEffect } from 'react'
 
 function App() {
+  const [classes, setClasses] = useState([])
+  const [selectedClass, setSelectedClass] = useState({})
+
+  const [topics, setTopics] = useState([])
+  const [selectedTopic, setSelectedTopic] = useState({})
+
+  const fetchClassList = async () => {
+    const fetchedClasses = await fetchClasses()
+    setClasses(fetchedClasses)
+  }
+
+  const fetchTopicList = async () => {
+    const fetchedTopics = await fetchTopics()
+    setTopics(fetchedTopics)
+  }
+
+  useEffect(() => {
+    fetchClassList()
+    fetchTopicList()
+  }, [classes, topics] )
 
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path='/' element={<HomePage/>} />
+        <Route path='/' element={<HomePage setSelectedClass={setSelectedClass} />} />
+        <Route path='/:classId' element={<EditUserClass selectedClass={selectedClass} fetchClassList={fetchClassList}/>} />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/sign-in' element={<SignInForm />} />
         <Route path='/create-topic' element={<TopicForm />} />
-        <Route path='/classes/create-class' element={<createClass />} />
-        <Route path='/classes/my-classes' element={<MyClasses classes={classes} setSelectedClass={setSelectedClass}/>} />
-        <Route path='/topics/my-topics' element={<MyTopics topics={topics} setSelectedTopic={setSelectedTopic}/>} />
-        <Route path='/classes/my-classes/:classId' element={<EditMyClass selectedClass={selectedClass} />} />
+        <Route path='/classes/create-class' element={<CreateClass />} />
+        <Route path='/classes/my-classes' element={<MyClasses classes={classes} setSelectedClass={setSelectedClass} />} />
+        <Route path='/topics/my-topics' element={<MyTopics topics={topics} setSelectedTopic={setSelectedTopic} />} />
+        <Route path='/classes/my-classes/:classId' element={<EditMyClass selectedClass={selectedClass} fetchClassList={fetchClassList} />} />
         <Route path='/topics/my-topics/:topicId' element={<EditMyTopic selectedTopic={selectedTopic} />} />
       </Routes>
     </>
